5f608668184c0c9e0df312828e8f5ce62997a9ff
ARYAA-6 <144935850+ARYAA-6@users.noreply.github.com>
2023-09-25 22:00:03 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 global.creator = 'Zeeoneofc'// yourname
 global.MONGO_DB_URI = "mongodb+srv://aryaa:arya5565@cluster0.51euhqa.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "aryaislami5565@gmail.com" //email
+global.email_password = "leldsutsmmwtzogj" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
